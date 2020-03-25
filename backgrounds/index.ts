cceb40c8f16fcb87e8b831093f34f9a09d88fe81
yury-n <yury.nedomolkin@gmail.com>
2020-03-26 02:29:01 +0400

    console.log replacement for dumping state

--- backgrounds/index.ts
+++ backgrounds/index.ts
@@ -1,6 +1,12 @@
 export default [
   {
     id: 1,
-    configValues: []
+    configValues: {
+      objectSize: 10,
+      objectDistance: 50,
+      padding: 10,
+      withRandomPosition: false,
+      randomizePositionStrength: 10
+    }
   }
 ];
--- saga.ts
+++ saga.ts
@@ -5,12 +5,31 @@ import es6promise from "es6-promise";
 import "isomorphic-unfetch";
 import { dumpState } from "./actions";
 import { AppState } from "./types/store";
+import { copyToClipboard } from "./utils";
 
 es6promise.polyfill();
 
 function* dumpStateSaga() {
+  const canvasWidth = yield select((state: AppState) => state.canvasWidth);
+  const canvasHeight = yield select((state: AppState) => state.canvasHeight);
+  const selectedLayoutId = yield select(
+    (state: AppState) => state.selectedLayoutId
+  );
+  const selectedObjectIds = yield select(
+    (state: AppState) => state.selectedObjectIds
+  );
   const configValues = yield select((state: AppState) => state.configValues);
-  console.log(configValues);
+  const configColors = yield select((state: AppState) => state.configColors);
+  copyToClipboard(
+    JSON.stringify({
+      canvasWidth,
+      canvasHeight,
+      selectedLayoutId,
+      selectedObjectIds,
+      configValues,
+      configColors
+    })
+  );
 }
 
 function* rootSaga() {
