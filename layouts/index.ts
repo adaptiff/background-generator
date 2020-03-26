7d620b52ae4b01c0edc8e311efd945fac275a1fe
lvovpeter <zuenfdm@ya.ru>
2020-03-26 03:20:30 +0300

    Fabric reinitialization disabled when colors changing Set objects origin to center and disable manual calculation of center Added angle parameter to objects

--- layouts/index.ts
+++ layouts/index.ts
@@ -1,6 +1,8 @@
 import layout1 from "./1";
 import layout2 from "./2";
 import layout3 from "./3";
-import layout4 from "./4.js";
+import layout4 from "./4";
+import layout5 from "./5";
+import layout6 from "./6";
 
-export default [layout1, layout2, layout3, layout4];
+export default [layout1, layout2, layout3, layout4, layout5, layout6];
